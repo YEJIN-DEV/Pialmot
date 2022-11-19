bf71cfdee7849fe3f8862f04881db9a9efbdce6a
Yuhwan Kim <asm6788@naver.com>
2022-11-19 19:41:39 +0900

    원본전송선택가능/앨범,종류,그룹 필드추가/함수 세분화/프론트엔드 프로토타입 작성

--- Back/Server/index.ts
+++ Back/Server/index.ts
@@ -19,10 +19,14 @@ function getRandomInt (min: number, max: number): number {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
+const midiPath = '../../Back/Melodia/output/'
+const mp3Path = '../../Back/Melodia/music/'
+
 enum musicKind {
   anime, // 애니 삽입곡
   original, // 오리지널
   single, // 싱글
+  unit, // 유닛
   special, // 특전
   album // 정규 앨범
 }
@@ -51,139 +55,97 @@ app.get('/', function (req, res) {
 */
 
 app.get('/music/:group', function (req, res) {
-  let path = '../../Back/Melodia/output/'
+  let groupPath = ''
   const group = groups[req.params.group as keyof typeof groups]
   switch (group) {
     case groups.us:
-      path += '' // TODO: 폴더명
+      groupPath = '' // TODO: 폴더명
       break
     case groups.aqours:
-      path += '' // TODO: 폴더명
+      groupPath = '' // TODO: 폴더명
       break
     case groups.nijigasaki:
-      path += 'Nijigasaki Gakuen School Idol Doukoukai/'
+      groupPath = 'Nijigasaki Gakuen School Idol Doukoukai'
       break
     case groups.liella:
-      path += 'Liella!/'
+      groupPath = 'Liella!'
       break
     default:
       res.status(404).send('그룹이 존재하지 않습니다.')
   }
 
-  const paths: string[] = []
-  for (const kind of req.query.kind as string[]) {
-    let tempPath = path
-    switch (kind) {
-      case 'anime':
-        switch (group) {
-          case groups.us:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.aqours:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.nijigasaki:
-            tempPath += '[2020-2022] Anime/'
-            break
-          case groups.liella:
-            tempPath += '[2021-2022] Anime/'
-        }
-        break
-      case 'original':
-        switch (group) {
-          case groups.us:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.aqours:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.nijigasaki:
-            tempPath += '[2020-2021] Original Song/'
-            break
-          case groups.liella:
-            tempPath += '[2021-2022] Original Songs/'
-        }
-        break
-      case 'single':
-        switch (group) {
-          case groups.us:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.aqours:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.nijigasaki:
-            res
-              .status(400)
-              .send(
-                'Nijigasaki Gakuen School Idol Doukoukai does not have single.'
-              )
-            break
-          case groups.liella:
-            tempPath += '[2021-2022] Anime/'
-        }
-        break
-      case 'unit':
-        switch (group) {
-          case groups.us:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.aqours:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.nijigasaki:
-            tempPath += '[2020-2022] Units/'
-            break
-          case groups.liella:
-            res.status(400).send('Liella! does not have unit.')
-        }
-        break
-      case 'special':
-        switch (group) {
-          case groups.us:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.aqours:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.nijigasaki:
-            tempPath += '[2021-2021] Special/'
-            break
-          case groups.liella:
-            tempPath += '[2021-2022] Special/'
-        }
-        break
-      case 'album':
-        switch (group) {
-          case groups.us:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.aqours:
-            tempPath += '' // TODO: 폴더명
-            break
-          case groups.nijigasaki:
-            tempPath += '[2018-2021] Albums/'
-            break
-          case groups.liella:
-            tempPath += '[2022-2022] Albums/'
-        }
-        break
-    }
-    paths.push(tempPath)
+  const kindPaths: { kind: musicKind; path: string }[] = []
+  if (!Array.isArray(req.query.kind)) {
+    req.query.kind = [req.query.kind as string]
   }
 
-  const result: Array<{ music: string; buffer: string }> = []
-  for (const path of paths) {
-    const dirs = getDirectories(path)
-    const dir = dirs[getRandomInt(0, dirs.length)]
-    const files = getFiles(path + dir)
-    const musicFile = files[getRandomInt(0, files.length)]
-    result.push({
-      music: musicFile.substring(0, musicFile.length - 4),
-      buffer: fs.readFileSync(path1.join(path, dir, musicFile), {
-        encoding: 'base64'
-      })
+  for (const kind of req.query.kind as Array<keyof typeof musicKind>) {
+    let kindPath = kindToFolder(musicKind[kind], group)
+    if (kindPath == undefined) {
+      res.status(404).send('존재하지 않는 종류입니다.')
+      return
+    } else {
+      kindPaths.push({ kind: musicKind[kind], path: kindPath })
+    }
+  }
+
+  const result: {
+    music: Array<{
+      name: string
+      group: groups
+      album: string
+      kind: musicKind
+      midi_buffer: string
+      mp3_buffer?: string
+    }>
+    questions: string[]
+  } = {
+    music: [],
+    questions: []
+  }
+
+  for (const kindPath of kindPaths) {
+    const { musicFile, dir } = randomMusic(groupPath, kindPath.path)
+    result.music.push({
+      name: musicFile.substring(0, musicFile.length - 4),
+      album: dir,
+      group: group,
+      kind: kindPath.kind,
+      midi_buffer: fs.readFileSync(
+        path1.join(midiPath, groupPath, kindPath.path, dir, musicFile),
+        {
+          encoding: 'base64'
+        }
+      ),
+      mp3_buffer:
+        req.query.original != undefined
+          ? fs.readFileSync(
+              path1.join(
+                mp3Path,
+                groupPath,
+                kindPath.path,
+                dir,
+                musicFile.substring(0, musicFile.length - 3) + 'mp3'
+              ),
+              {
+                encoding: 'base64'
+              }
+            )
+          : undefined
     })
+  }
+
+  for (let i = 0; i < 5; i++) {
+    const kindPath = kindToFolder(
+      getRandomInt(musicKind.anime, musicKind.album),
+      group
+    )
+    if (kindPath !== undefined) {
+      let temp = randomMusic(groupPath, kindPath).musicFile
+      result.questions.push(temp.substring(0, temp.length - 4))
+    } else {
+      i--
+    }
   }
 
   res.json(result)
@@ -192,3 +154,110 @@ app.get('/music/:group', function (req, res) {
 server.listen(8000, function () {
   console.log('서버ON')
 })
+
+function kindToFolder (kind: musicKind, group: groups): string | undefined {
+  let path = ''
+  switch (kind) {
+    case musicKind.anime:
+      switch (group) {
+        case groups.us:
+          path = '' // TODO: 폴더명
+          break
+        case groups.aqours:
+          path = '' // TODO: 폴더명
+          break
+        case groups.nijigasaki:
+          path = '[2020-2022] Anime'
+          break
+        case groups.liella:
+          path = '[2021-2022] Anime'
+      }
+      break
+    case musicKind.original:
+      switch (group) {
+        case groups.us:
+          path = '' // TODO: 폴더명
+          break
+        case groups.aqours:
+          path = '' // TODO: 폴더명
+          break
+        case groups.nijigasaki:
+          path = '[2020-2021] Original Song'
+          break
+        case groups.liella:
+          path = '[2021-2022] Original Songs'
+      }
+      break
+    case musicKind.single:
+      switch (group) {
+        case groups.us:
+          path = '' // TODO: 폴더명
+          break
+        case groups.aqours:
+          path = '' // TODO: 폴더명
+          break
+        case groups.nijigasaki:
+          return undefined
+        case groups.liella:
+          path = '[2021-2022] Singles'
+      }
+      break
+    case musicKind.unit:
+      switch (group) {
+        case groups.us:
+          path = '' // TODO: 폴더명
+          break
+        case groups.aqours:
+          path = '' // TODO: 폴더명
+          break
+        case groups.nijigasaki:
+          path = '[2020-2022] Units/'
+          break
+        case groups.liella:
+          return undefined
+      }
+      break
+    case musicKind.special:
+      switch (group) {
+        case groups.us:
+          path = '' // TODO: 폴더명
+          break
+        case groups.aqours:
+          path = '' // TODO: 폴더명
+          break
+        case groups.nijigasaki:
+          path = '[2021-2021] Special'
+          break
+        case groups.liella:
+          path = '[2021-2022] Special'
+      }
+      break
+    case musicKind.album:
+      switch (group) {
+        case groups.us:
+          path = '' // TODO: 폴더명
+          break
+        case groups.aqours:
+          path = '' // TODO: 폴더명
+          break
+        case groups.nijigasaki:
+          path = '[2018-2021] Albums'
+          break
+        case groups.liella:
+          path = '[2022-2022] Albums'
+      }
+      break
+  }
+  return path
+}
+
+function randomMusic (
+  groupPath: string,
+  kindPath: string
+): { musicFile: string; dir: string } {
+  const dirs = getDirectories(path1.join(midiPath, groupPath, kindPath))
+  const dir = dirs[getRandomInt(0, dirs.length)]
+  const files = getFiles(path1.join(midiPath, groupPath, kindPath, dir))
+  const musicFile = files[getRandomInt(0, files.length)]
+  return { musicFile, dir }
+}
--- Front/client.js
+++ Front/client.js
@@ -1,3 +1,33 @@
-function getRandMusic(){
-    fetch('/music')
+var before = 0;
+var snd = new Audio();
+
+function getRandMusic(group){
+    fetch('http://112.164.62.41:8000/music/'+group+'?kind=anime&original')
+    .then((response) => response.json())
+    .then((data) => {
+        MIDI.Player.loadFile("data:audio/midi;base64,"+data[0].music.midi_buffer, function() {
+            document.getElementById('current_music').innerText = data[0].music.name;
+            MIDI.Player.start();
+            before = new Date();
+            snd.src = "data:audio/mp3;base64," +data[0].music.mp3_buffer;
+            snd.volume = 0.05;
+            snd.load();
+        });
+    });
 }
+
+function playOriginal(){
+    if (snd.paused){
+        snd.currentTime = MIDI.Player.currentTime/1000;
+        snd.play();
+    } else{
+        snd.pause();
+    }
+}
+
+window.onload = function () {
+    MIDI.loadPlugin({
+        soundfontUrl: "https://gleitz.github.io/midi-js-soundfonts/MusyngKite/",
+        instrument: "gunshot"
+    });
+};
