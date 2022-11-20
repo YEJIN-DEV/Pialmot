9c4dd7e5df90019ac418fb5631c4464153332dc0
Yuhwan Kim <asm6788@naver.com>
2022-11-20 18:23:02 +0900

    svelte 프론트엔드 초안/gitignore 구조 변경

---
+++ Front/Pialmot/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import * as client from "./client";
+</script>
+
+<svelte:head>
+	<script type="text/javascript" src="//www.midijs.net/lib/midi.js"></script>
+</svelte:head>
+
+<main>
+	<p id="current_music">재생:없다맨이야</p>
+	<button on:click={() => client.getRandMusic("liella")}>리에라 재생ㄱ</button
+	>
+	<button on:click={() => client.getRandMusic("nijigasaki")}
+		>니지동 재생ㄱ</button
+	>
+	<button on:click={() => client.playOriginal()}>원곡 재생/정지</button>
+	<br />
+	<select on:change={() => client.onAnswer()} id="questions" size="5" />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
--- Front/Pialmot/src/client.js
+++ Front/Pialmot/src/client.js
@@ -1,21 +1,19 @@
 var before = 0;
 var snd = new Audio();
-var answer = '';
-var group = 0
+export var answer = '';
+export var group = 0
 
-function getRandMusic(group) {
+export function getRandMusic(group) {
     fetch(`http://112.164.62.41:8000/music/${group}?kind=anime&original`)
         .then((response) => response.json())
         .then((data) => {
-            MIDI.Player.loadFile("data:audio/midi;base64," + data.midi_buffer, function () {
-                document.getElementById('current_music').innerText = data.name;
-                MIDI.Player.start();
-                snd.src = "data:audio/mp3;base64," + data.mp3_buffer;
-                snd.volume = 0.05;
-                snd.load();
-            });
-
+            MIDIjs.play("data:audio/midi;base64," + data.midi_buffer)
+            document.getElementById('current_music').innerText = data.name;
+            snd.src = "data:audio/mp3;base64," + data.mp3_buffer;
+            snd.volume = 0.05;
+            snd.load();
             before = new Date();
+
             let selectbox = document.getElementById('questions')
             while (selectbox.options.length > 0) {
                 selectbox.remove(0);
@@ -32,7 +30,7 @@ function getRandMusic(group) {
         });
 }
 
-function playOriginal() {
+export function playOriginal() {
     if (snd.paused) {
         snd.currentTime = MIDI.Player.currentTime / 1000;
         snd.play();
@@ -41,7 +39,7 @@ function playOriginal() {
     }
 }
 
-function onAnswer() {
+export function onAnswer() {
     let selectbox = document.getElementById('questions');
     let selected = selectbox.options[selectbox.selectedIndex].value;
     if (selected == answer) {
@@ -62,10 +60,3 @@ function onAnswer() {
         alert('오답입니다!');
     }
 }
-
-window.onload = function () {
-    MIDI.loadPlugin({
-        soundfontUrl: "https://gleitz.github.io/midi-js-soundfonts/MusyngKite/",
-        instrument: "gunshot"
-    });
-};
