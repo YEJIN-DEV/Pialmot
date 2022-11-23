5f970ee2b00357a4e92969b9acce0edeb5eb2f7c
Yuhwan Kim <asm6788@naver.com>
2022-11-23 11:29:05 +0900

    자동재생을 위해 muted 태그 추가

--- Front/Pialmot/src/main.svelte
+++ Front/Pialmot/src/main.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { push, pop, replace } from "svelte-spa-router";
+    import { push } from "svelte-spa-router";
 </script>
 
 <svelte:head>
@@ -13,7 +13,7 @@
 <body>
     <div class="video-container">
         <!-- svelte-ignore a11y-media-has-caption -->
-        <video autoplay loop>
+        <video muted autoplay loop>
             <source src="main.webm" />
         </video>
     </div>
