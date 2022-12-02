adc7d49eb94aa1c09a5be5ec3b469614a9560bb7
Yuhwan Kim <asm6788@naver.com>
2022-12-02 15:00:53 +0900

    Revert "HTTP/2 지원"
    This reverts commit 04bd9794e0aafbd486bd38f9b4ebc70484678d3c.

--- Back/Server/index.ts
+++ Back/Server/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import spdy from 'spdy'
+import https from 'https'
 import http from 'http'
 import fs from 'fs'
 import path1 from 'path'
@@ -51,7 +51,7 @@ enum groups {
 const app = express()
 app.use(express.text())
 app.use(morgan('common'));
-const httpsServer = spdy.createServer(credentials, app)
+const httpsServer = https.createServer(credentials, app)
 /*
 오류:
 니지동: 싱글
